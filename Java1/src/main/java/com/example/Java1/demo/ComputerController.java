537319fd12cbd5f7e30adcc1829cdfc948c62476
inoricode28 <inori237@gmail.com>
2026-03-02 00:53:49 -0500

    completado

--- Java1/src/main/java/com/example/Java1/demo/ComputerController.java
+++ Java1/src/main/java/com/example/Java1/demo/ComputerController.java
@@ -1,7 +1,5 @@
 package com.example.Java1.demo;
 
-import com.example.Java1.demo.Computer;
-import com.example.Java1.demo.Sale;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
